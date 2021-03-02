2701f87246665a8325a6baa2a1ebc8b00ae84789
msalda <msalda53@gmail.com>
2021-03-02 17:40:38 -0600

    intro y variables

---
+++ react/02-intro-javascript/src/index.js
@@ -0,0 +1,7 @@
+console.log("hola mundoo")
+//variables y constantes
+const nombre = "mario";
+let apellido = "saldaña";
+let valor_dado = 2;
+valor_dado =3;
+console.log(nombre,apellido,valor_dado);
